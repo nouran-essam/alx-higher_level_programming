9f039782a38867c9537eb5f16ebba655239a3853
nouran-essam <nouranessam885@gmail.com>
2024-08-11 13:08:33 -0700

    d

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = []
-    for number in my_list:
-        if (number % 2) == 0:
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return(new_list)
+    blist = [a % 2 == 0 for a in my_list]
+    return blist
