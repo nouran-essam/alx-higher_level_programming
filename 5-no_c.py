def09ce26a50341c65e37009189bc4b6830e2116
nouran-essam <nouranessam885@gmail.com>
2024-08-11 12:31:26 -0700

    d

--- 5-no_c.py
+++ 5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = ""
-    for elm in my_string:
-        if elm != 'C' and elm != 'c':
-            new_str += elm
-    return (new_str)
+    s = my_string
+    s = s.translate({ord('c'): None})
+    s = s.translate({ord('C'): None})
+    return (s)
