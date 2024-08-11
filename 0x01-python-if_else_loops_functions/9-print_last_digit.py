c84103ff30c6d73a38b15cce5b96508da4edc1f0
nouran-essam <nouranessam885@gmail.com>
2024-08-11 11:25:04 -0700

    d

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        number = number * -1
-    a = number % 10
-    print("{:d}".format(a), end='')
-    return(a)
+        last = (-number % 10)
+    elif number >= 0:
+        last = number % 10
+    print("{:d}".format(last), end="")
+    return(last)
