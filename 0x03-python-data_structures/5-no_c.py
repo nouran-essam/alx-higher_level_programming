573678a6467880913897bf9ee57e6612960c7a92
nouran-essam <nouranessam885@gmail.com>
2024-08-11 13:01:52 -0700

    d

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    listofchars = list(my_string)
-    for char in listofchars:
-        if char == 'c' or char == 'C':
-            listofchars.remove(char)
-    return("".join(listofchars))
+    new_str = ""
+    for elm in my_string:
+        if elm != 'C' and elm != 'c':
+            new_str += elm
+    return new_str
