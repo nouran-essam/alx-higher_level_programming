9fbed18bd880dc36692c2c1bd24af221d1d0c8c6
nouran-essam <nouranessam885@gmail.com>
2024-08-11 11:46:21 -0700

    d

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    import sys
-    from calculator_1 import add, sub, mul,
-    num_arg = len(sys.argv) - 1
-    if num_arg < 3:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    a, operator, b = sys.argv[1:]
-    if operator not in "+-*/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    print("{:s} {:s} {:s} = ".format(a, operator, b), end="")
-    for ope, func in [('+', add), ('-', sub), ('*', mul), ('/', div)]:
-        if operator == ope:
-            print("{:d}".format(func(int(a), int(b))))
+
+    ops = ["+", "-", "*", "/"]
+    fcs = [add, sub, mul, div]
+    for i in range(0, 4):
+        if argv[2] == ops[i]:
+            a = int(argv[1])
+            b = int(argv[3])
+            print("{:d} {:s} {:d} = {:d}".format(a, argv[2], b, fcs[i](a, b)))
+            exit(0)
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
