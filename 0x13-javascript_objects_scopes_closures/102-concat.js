f9c33217f1fedc6feafa7eb57159cece8ab9fde5
nouran-essam <nouranessam885@gmail.com>
2024-09-21 12:27:53 -0700

    d

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,8 +1,19 @@
 #!/usr/bin/node
-// script that concats 2 files
-
-const args = process.argv.slice(2);
-const file = require('fs');
-const contentA = file.readFileSync('./' + args[0]);
-const contentB = file.readFileSync('./' + args[1]);
-file.writeFileSync('./' + args[2], contentA + contentB);
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+const newfile = process.argv[4];
+const fs = require('fs');
+const data1 = fs.readFileSync(file1, (err, data) => {
+  if (err) throw err;
+  return (data);
+});
+const data2 = fs.readFileSync(file2, (err, data) => {
+  if (err) throw err;
+  return (data);
+});
+fs.appendFile(newfile, data1, function (err) {
+  if (err) throw err;
+});
+fs.appendFile(newfile, data2, function (err) {
+  if (err) throw err;
+});
