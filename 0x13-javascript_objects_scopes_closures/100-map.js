72443581b0446e0f2f0272428d7c5d197de4bf6a
nouran-essam <nouranessam885@gmail.com>
2024-09-21 12:24:27 -0700

    d

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+const list = require('./100-data').list;
+
+console.log(list);
+const newList = list.map(function (x, idx) {
+  return (x * idx);
+});
+console.log(newList);
